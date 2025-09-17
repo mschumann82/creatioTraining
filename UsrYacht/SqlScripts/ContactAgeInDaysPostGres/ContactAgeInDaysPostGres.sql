d7dec8b9a3d8244373d7ca2a7db97d0a0ef7c99b
Michael <schumann82@gmail.com>
2025-09-17 13:52:29 -0700

    updated

---
+++ UsrYacht/SqlScripts/ContactAgeInDaysPostGres/ContactAgeInDaysPostGres.sql
@@ -0,0 +1,6 @@
+create or replace view public."UsrVwContactAgeDays"
+as
+select "Id" as "UsrId", "Name" as "UsrName", "BirthDate" as "UsrBirthDate",
+CURRENT_DATE -"BirthDate" as "UsrAgeDays",
+"Id" as "UsrContactId"
+from public."Contact"
